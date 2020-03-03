23a95dbb36f749b81b76323ac14a25bf2abb986d
arbuzov <arbuzov@code-artel.com>
2020-03-03 21:16:44 +0300

    1. language gender from ru to en 2. comments to en 3. result file

--- parser_toloka.py
+++ parser_toloka.py
@@ -2,6 +2,8 @@ import pandas as pd
 import requests
 import mimetypes
 from pathlib import Path
+
+
 # import checker
 
 
@@ -10,58 +12,70 @@ def get_media_type(filename):
     return mime_start.split('/')[0]
 
 
-# Указываем ключ к API, а также ID пула первого и второго задания
+
 path_to_save_video = r'C:\Users\Arbuzov\Desktop\Work_CVT'
-TOLOKA_OAUTH_TOKEN = "AgAAAAA5OljPAACtpYsQUstOSEWDs_k09mbI1M4"
-#TOLOKA_OAUTH_TOKEN = "AgAAAAA8qXcxAAIbujGo7DDpoko2ty3hwvHze6Q"  #sandbox
-#URL_API = "https://sandbox.toloka.yandex.ru/api/v1/"
-URL_API = "https://toloka.yandex.ru/api/v1/"
+
+# Specify the key to the API, as well as the pool ID of the first and second jobs
+# Main work TOLOKA
+# TOLOKA_OAUTH_TOKEN = "AgAAAAA5OljPAACtpYsQUstOSEWDs_k09mbI1M4"
+# URL_API = "https://toloka.yandex.ru/api/v1/"
+
+# SANDBOX
+TOLOKA_OAUTH_TOKEN = "AgAAAAA8qXcxAAIbujGo7DDpoko2ty3hwvHze6Q"
+URL_API = "https://sandbox.toloka.yandex.ru/api/v1/"
+
 HEADERS = {
     "Authorization": "OAuth %s" % TOLOKA_OAUTH_TOKEN,
     "Content-Type": "application/JSON",
 }
 
-########################### сделать проверку по проектам???
+########################### do check on projects ???
 
-# Получаем список пулов, в текущем проекте
+# Get the list of pools in the current project.
 url_pools = (
         URL_API + "pools?"
 )
 POOLS_ID = requests.get(url_pools, headers=HEADERS).json()["items"]
-k = 1  # для удобства подсчета кол-ва пулов
+k = 1  # for the convenience of counting the number of pools
 db = []
 for item in POOLS_ID:
     pool_id = item['id']
 
-    print(pool_id + " %s" %k)
+    print(pool_id + " %s" % k)
     k += 1
 
-    # Получаем список всех ответов из i-ого пула, которые ждут проверки
+    # We get a list of all the answers from the i-th pool that are waiting for verification
     url_assignments = (
             URL_API + "assignments/?status=SUBMITTED&pool_id=%s" % pool_id
     )
     submitted_tasks = requests.get(url_assignments, headers=HEADERS).json()["items"]
-    # print(submitted_tasks)
+    print(submitted_tasks)
 
     for task in submitted_tasks:
-        # print(task)
+        print(task)
         try:
             url_file = (
-                    URL_API + "attachments/%s" % task['solutions'][0]['output_values']['video']
+                # URL_API + "attachments/%s" % task['solutions'][0]['output_values']['video'] # main
+                URL_API + "attachments/%s" % task['solutions'][0]['output_values']['photo']  # sandbox
             )
         except:
             continue
 
         info_file = requests.get(url_file, headers=HEADERS).json()
 
-        # print(info_file)
+        print(info_file)
 
         file_name = info_file['name']
 
-        # проверка файла на видео с помощью толоки
+        # checking file on video using shredding
         video = (info_file['media_type'].split('/')[0] == "video")
 
-        file_download = requests.get(url_file + "/download", headers=HEADERS)  # делаем запрос  и скачиваем файл
+        if task['solutions'][0]['output_values']['gender'] == "муж.":
+            gender = "male"
+        else:
+            gender = "female"
+
+        # file_download = requests.get(url_file + "/download", headers=HEADERS)  # download file with get request
         db.append(
             {
                 "pool_id": task['pool_id'],
@@ -71,23 +85,23 @@ for item in POOLS_ID:
                 "glasses": task['solutions'][0]['output_values']['glasses'],
                 "age": task['solutions'][0]['output_values']['age'],
                 "photo": task['solutions'][0]['output_values']['photo'],
-                "gender": task['solutions'][0]['output_values']['gender'],
+                "gender": gender,
                 "file_name": file_name,
                 "media_type": info_file["media_type"],
                 "local_path": path_to_save_video,
                 "is_video": video,
             }
         )
-        #else:
-            ############ сделать логирование loging.info
+        # else:
+        ############ make loging.info
         #    json_check = {
         #        "status": "REJECTED",
         #        "public_comment": "Загруженный файл не является видео!",
         #    }
         #    requests.patch(URL_API + "assignments/%s" % task['id'], headers=HEADERS, json=json_check)
 
-        # ---------- локальная проверка файлов ----------
-        # file_download = requests.get(url_file + "/download", headers=HEADERS)  # делаем запрос
+        # ---------- local file check ----------
+        # file_download = requests.get(url_file + "/download", headers=HEADERS)  # download file with get request
         # # open method to open a file on your system and write the contents
         # with open((path_to_save_video + r'\%s' % file_name), "wb") as file:
         #     file.write(file_download.content)
@@ -115,4 +129,4 @@ for item in POOLS_ID:
         #     }
         #     requests.patch(URL_API + "assignments/%s" %task['id'], headers=HEADERS, json=json_check)
 
-pd.DataFrame(db).to_csv("result.csv", sep=";", encoding='cp1251')
+pd.DataFrame(db).to_csv("result.tsv", index=False, sep="\t", encoding='utf-8')
