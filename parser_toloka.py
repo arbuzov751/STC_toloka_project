84b067ad577a3f5f55284f60da0c2b50df56575a
arbuzov <arbuzov@code-artel.com>
2020-03-10 21:25:06 +0300

    Add check with types + some fixes

--- parser_toloka.py
+++ parser_toloka.py
@@ -3,8 +3,16 @@ import requests
 import mimetypes
 from pathlib import Path
 
-
 # import checker
+
+
+types = [".3g2", ".3gp", ".3gp2", ".3gpp", ".3gpp2",
+         ".asf", ".asx", ".avi", ".bin", ".dat", "drv",
+         ".f4v", ".flv", ".gtp", ".h264", ".m4v", ".mkv",
+         ".mod", ".moov", ".mov", ".mp4", ".mpeg", ".mpg",
+         ".mts", ".rm", ".rmvb", ".spl", ".srt", ".stl",
+         ".swf", ".ts", ".vcd", ".vid", ".vob", ".webm",
+         ".wm", ".wmv", ".yuv"]  # types all popular video files
 
 
 def get_media_type(filename):
@@ -12,10 +20,10 @@ def get_media_type(filename):
     return mime_start.split('/')[0]
 
 
-
-path_to_save_video = r'C:\Users\Arbuzov\Desktop\Work_CVT'
+path_to_save_video = r'C:\STC_toloka_project'
 
 # Specify the key to the API, as well as the pool ID of the first and second jobs
+
 # Main work TOLOKA
 # TOLOKA_OAUTH_TOKEN = "AgAAAAA5OljPAACtpYsQUstOSEWDs_k09mbI1M4"
 # URL_API = "https://toloka.yandex.ru/api/v1/"
@@ -29,19 +37,20 @@ HEADERS = {
     "Content-Type": "application/JSON",
 }
 
-########################### do check on projects ???
+########################### do check with projects ???
 
 # Get the list of pools in the current project.
 url_pools = (
-        URL_API + "pools?"
+        URL_API + "pools?status=OPEN"  # need check for status?
 )
 POOLS_ID = requests.get(url_pools, headers=HEADERS).json()["items"]
 k = 1  # for the convenience of counting the number of pools
-db = []
+
 for item in POOLS_ID:
+    db = []
     pool_id = item['id']
 
-    print(pool_id + " %s" % k)
+    print(f"{pool_id} {k}")
     k += 1
 
     # We get a list of all the answers from the i-th pool that are waiting for verification
@@ -49,33 +58,45 @@ for item in POOLS_ID:
             URL_API + "assignments/?status=SUBMITTED&pool_id=%s" % pool_id
     )
     submitted_tasks = requests.get(url_assignments, headers=HEADERS).json()["items"]
-    print(submitted_tasks)
+    # print(submitted_tasks)
 
     for task in submitted_tasks:
-        print(task)
+        # print(task)
         try:
             url_file = (
                 # URL_API + "attachments/%s" % task['solutions'][0]['output_values']['video'] # main
-                URL_API + "attachments/%s" % task['solutions'][0]['output_values']['photo']  # sandbox
+                    URL_API + "attachments/%s" % task['solutions'][0]['output_values']['photo']  # sandbox
             )
         except:
             continue
 
         info_file = requests.get(url_file, headers=HEADERS).json()
 
-        print(info_file)
+        # print(info_file)
 
         file_name = info_file['name']
+        print(file_name)
 
         # checking file on video using shredding
-        video = (info_file['media_type'].split('/')[0] == "video")
+        video = int(info_file['media_type'].split('/')[0] == "video")
+        if info_file['media_type'].split('/')[0] == "application":
+            extention = Path(file_name)
+            extention.suffix.lower()
+            if extention in types:
+                video = 1
 
         if task['solutions'][0]['output_values']['gender'] == "муж.":
             gender = "male"
         else:
             gender = "female"
 
-        # file_download = requests.get(url_file + "/download", headers=HEADERS)  # download file with get request
+        if video == 1:
+            # pass
+            file_download = requests.get(url_file + "/download", headers=HEADERS)  # download file with get request
+            # open method to open a file on your system and write the contents
+            with open((path_to_save_video + r'\%s' % file_name), "wb") as file:
+                file.write(file_download.content)
+
         db.append(
             {
                 "pool_id": task['pool_id'],
@@ -129,4 +150,4 @@ for item in POOLS_ID:
         #     }
         #     requests.patch(URL_API + "assignments/%s" %task['id'], headers=HEADERS, json=json_check)
 
-pd.DataFrame(db).to_csv("result.tsv", index=False, sep="\t", encoding='utf-8')
+    pd.DataFrame(db).to_csv(f"pool_{pool_id}.tsv", index=False, sep="\t", encoding='utf-8')
